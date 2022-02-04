2c873d4733b3f39e0aecdd6ff80ec64d2358a489
juanjocr96 <jcarrascorodriguez.guadalupe@fundacionloyola.net>
2022-02-04 12:32:30 +0100

    Menú y estilos.

--- src/index.js
+++ src/index.js
@@ -4,35 +4,51 @@ import './index.css';
 class Crud extends React.Component{
     render(){
         return(
-            <div>
-                 <Menu 
-        
+            <div id="contenedor">
+                 <Menu
+
                  />
-                 <Formulario 
-                
+                 <Formulario
+
                 />
-            </div>     
+            </div>
         )
-       
+
     }
 }
 class Menu extends React.Component{
     render(){
         return(
-            <aside>Estoy</aside>
+            <aside>
+              <SubMenu
+
+              />
+            </aside>
         );
     }
 
 }
+
+class SubMenu extends React.Component{
+    render(){
+      return(
+          <ol>
+            <li>Jesuitas Familia: <a href="https://jesuitasfamilia.es/">Saber más</a></li>
+            <li>Camino Ignaciano: <a href="https://caminoignaciano.org/es/">Ir ahora</a></li>
+            <li>Fundación Loyola: <a href="https://fundacionloyola.com/vguadalupe/">Quiénes somos</a></li>
+          </ol>
+      )
+    }
+}
+
 class Formulario extends React.Component{
     render(){
         return(
-            <div>
+            <div id="contenedorForm">
                 <form>
-                <input type="text" placeholder='llevate esta'/>
                 </form>
             </div>
-            
+
         );
     }
 
