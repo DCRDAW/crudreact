1ba56ae27d4f15b99ea97200e51ad39297cb39ba
DCRDAW <dcarrionrodriguez.guadalupe@alumnado.fundacionloyola.net>
2022-02-05 21:34:03 +0100

    Resultados

--- src/index.js
+++ src/index.js
@@ -42,27 +42,66 @@ class SubMenu extends React.Component{
 }
 
 class Formulario extends React.Component{
-    render(){
-        return(
-            <div id='contenedorForm'>
-                <form>
-                    <h1>Formulario</h1>
-                    <label>Surrender al 20</label>
-                    <input type="text"/>
-                    <label>Surrender al 20</label>
-                    <input type="text"/>
-                    <label>Surrender al 20</label>
-                    <input type="text"/>
-                    <input type="button" value="Enviar" />
-                </form>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComponent: false,
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
+  _onButtonClick() {
+    this.setState({
+      showComponent: true,
+    });
+  }
+  handleSumbit(event){
+    event.preventDefault();  
+  }
+  render(){
+      return(
+          <div id='contenedorForm'>
+              <form onSubmit={this.handleSumbit}>
+                  <h1>Formulario</h1>
+                  <label>Surrender al 20</label>
+                  <input type="text"/>
+                  <label>Surrender al 20</label>
+                  <input type="text"/>
+                  <label>Surrender al 20</label>
+                  <input type="text"/>
+                  <button onClick={this._onButtonClick}>Button</button>
+                  
+              </form>
+              {this.state.showComponent ?
+                    <Resultado /> :
+                    null
+                  }
+          </div>
 
-        );
-    }
+      );
+  }
 
 }
 
+function Numaleat(){
+  return Math.floor(Math.random() * (4 - 0 + 1) + 0)
+}
+function Resultado(){
+  let numaleat= Math.floor(Math.random() * (5 - 3 + 1) + 3)
+  let array=[]
+  let titulos= ['AlFin','Acabo','Esto','Madre','Mia']
+  let textos= ['React','Te','Voy','A','*Censurado*']
+  for (let i = 0; i < numaleat; i++) {
+    array.push(
+      <div key={i + 1} className="cuadro">
+        <p className='titulo'>{titulos[Numaleat()]}</p>
+        <p>{textos[Numaleat()]}</p>
+      </div>)
+  }
+  return (
+    <div className='Resultados'>{array}</div>
+  )
 
+}
 
 
 
@@ -70,3 +109,4 @@ ReactDOM.render(
     <Crud />,
     document.getElementById('root')
   );
+
